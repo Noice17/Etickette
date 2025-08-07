743475ba9e8ca35739324cee960f226b17970d81
koralsus <karl_matthew_carasus@dlsu.edu.ph>
2025-08-07 14:56:55 +0800

    pdf reports (ikasampung tulak)

--- src/main/java/com/sts/Etickette/controller/ReportController.java
+++ src/main/java/com/sts/Etickette/controller/ReportController.java
@@ -4,10 +4,12 @@ import com.sts.Etickette.service.PdfReportService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@PreAuthorize("hasRole('ADMIN')")
 @RestController
 @RequestMapping("/api/reports")
 public class ReportController {
